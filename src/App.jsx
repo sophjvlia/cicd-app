fcf7201eba0bc99b02922caf8d708cefadce0b00
Sophie Crabtree <sophiejulianacrabtree@gmail.com>
2025-01-01 22:50:08 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react'
+import { Button, Container, Form } from 'react-bootstrap'
+
+function App() {
+  const [email, setEmail] = useState('');
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const checkFormValidity = () => {
+    const form = document.querySelector('form');
+    setIsFormValid(form.checkValidity());
+  };
+
+  useEffect(() => {
+    checkFormValidity();
+  }, []);
+
+  return (
+    <Container className="my-5">
+      <h1>Form Submission</h1>
+      <Form className="my-4" onChange={checkFormValidity}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            data-testid="emailInput"
+          />
+        </Form.Group>
+        <Button
+          type="submit"
+          className="btn btn-primary"
+          disabled={!isFormValid}
+          data-testid="submitButton"
+        >
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  )
+}
+
+export default App
---
+++ src/App.test.jsx
@@ -0,0 +1,15 @@
+import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import App from './App'
+
+test("App form submission", async () => {
+    render(<App/>);
+
+    const emailInput = screen.getByTestId("emailInput");
+    const submitButton = screen.getByTestId("submitButton");
+
+    await userEvent.type(emailInput, "sigmaschool@gmail.com");
+
+    expect(submitButton.disabled).toBe(false);
+});
